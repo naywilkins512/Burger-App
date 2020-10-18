4c0980879fa199440d201635acf77e45ad907b03
naywilkins512 <nathanwilkins01@gmail.com>
2020-10-18 09:59:04 -0500

    adds burgers controller folder with completed router not tested

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -3,6 +3,43 @@ let router = express.Router();
 let burger = require("../models/burger.js");
 
 
+router.get("/", function (req, res) {
+    burger.selectAll(function (data) {
+        let hbsObject = {
+            burgers: data
+        };
+        console.log(hbsObject);
+        res.render("index", hbsObject);
+    });
+});
+
+router.post("/api/burgers", function (req, res) {
+    burger.insertOne([
+        "burger_name", "devoured"
+    ], [
+        req.body.burger_name, req.body.devoured
+    ], function (result) {
+        // Send back the ID of the new quote
+        res.json({ id: result.insertId });
+    });
+});
+
+
+router.put("/api/burgers/:id", function (req, res) {
+    let condition = "id = " + req.params.id;
+
+    console.log("condition", condition);
+
+    burger.updateOne({
+        devoured: req.body.devoured
+    }, condition, function (result) {
+        if (result.changedRows == 0) {
+            return res.status(404).end();
+        } else {
+            res.status(200).end();
+        }
+    });
+});
 module.exports = router;
 
 
