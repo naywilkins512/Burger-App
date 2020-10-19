8d0120a4415d66b22ecfa5abfcfce939f83bdb1d
naywilkins512 <nathanwilkins01@gmail.com>
2020-10-18 23:20:54 -0500

    changed connection.js

--- config/connection.js
+++ config/connection.js
@@ -1,19 +1,19 @@
 let mysql = require("mysql");
 
-let connection = mysql.createConnection({
-  host: "localhost",
-  port: 3306,
-  user: "root",
-  password: "Hikes479",
-  database: "burgers_db"
-});
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL)
+} else {
+  connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "Hikes479",
+    database: "burgers_db"
+  });
 
-connection.connect(function(err) {
-  if (err) {
-    console.error("error connecting: " + err.stack);
-    return;
-  }
-  console.log("connected as id " + connection.threadId);
-});
+}
+
+
+connection.connect();
 
 module.exports = connection;
