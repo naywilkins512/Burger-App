ebcee86f1ce6adc272d4f5b2193fbb7f5f5f1f8c
naywilkins512 <nathanwilkins01@gmail.com>
2020-10-21 19:11:03 -0600

    fixes burger img path

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ let app = express();
 
 // Serve static content for the app from the "public" directory in the application directory.
 app.use(express.static("public"));
-app.use(express.static('views/images')); 
+// app.use(express.static('views/images')); 
 // Parse application body as JSON
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
